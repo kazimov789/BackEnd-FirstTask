11ab5c527e0f648769f347f07db572acb0d82f14
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-04-18 18:38:58 +0400

    Add files via upload

---
+++ FirstBackEndTask/FirstBackEndTask/Controllers/Main.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstBackEndTask.Controllers
+{
+    public class Main:Controller
+    {
+        public ViewResult detail() { return View(); }
+        public ViewResult contact()
+        {
+            return View();
+        }
+        public ViewResult about()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstBackEndTask/FirstBackEndTask/Controllers/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstBackEndTask.Controllers
+{
+    public class Product:Controller
+    {
+        public ContentResult detail(int id)
+        {
+            return Content(id.ToString()) ;
+        }
+    }
+}
